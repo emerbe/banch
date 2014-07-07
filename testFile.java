10943d75aefc9097db436ae7f2ba8c7743b6896f
emerbe <michalbucki@gmail.com>
2014-07-07 15:49:20 +0200

    Task 1 complete

--- testFile.java
+++ testFile.java
@@ -2,6 +2,7 @@
 public class testFile {
 
 	public static void main(String[] args) {
-		System.out.println("TASK1");			
+		System.out.println("TASK1");
+		System.out.println("TASK1 COMPLETE");	
 	}
 }
