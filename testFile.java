35c5b3004dd7b629dc66a75b3d51075b4bd31728
emerbe <michalbucki@gmail.com>
2014-07-07 15:25:44 +0200

    TestCommit3

--- testFile.java
+++ testFile.java
@@ -3,6 +3,7 @@ public class testFile {
 
 	public static void main(String[] args) {
 			System.out.println("TEST");
-	System.out.println("TEST");
+			System.out.println("TEST");
+			System.out.println("TEST");
 	}
 }
