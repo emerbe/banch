c22f6bdcb76070205111660a7848c2049511168c
emerbe <michalbucki@gmail.com>
2014-07-07 15:41:59 +0200

    Usunięcie komentarzy

--- testFile.java
+++ testFile.java
@@ -3,9 +3,5 @@ public class testFile {
 
 	public static void main(String[] args) {
 		System.out.println("TEST");			
-		System.out.println("TEST");
-		System.out.println("MAIN BRANCH");
-		System.out.println("TEST");
-		System.out.println("TEST");	
 	}
 }
