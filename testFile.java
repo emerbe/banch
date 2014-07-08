295286a79fbd249b0491939e7c59ce6bcd5441e5
emerbe <michalbucki@gmail.com>
2014-07-08 11:09:17 +0200

    Before revert

--- testFile.java
+++ testFile.java
@@ -2,10 +2,6 @@
 public class testFile {
 
 	public static void main(String[] args) {
-		System.out.println("TASK1");
-		System.out.println("TASK1 COMPLETE");	
-		System.out.println("3");
-		System.out.println("EPAM GIT");
-		System.out.println("EPAM GIT2");
+		System.out.println("BEFORE REVERT");
 	}
 }
