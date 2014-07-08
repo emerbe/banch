4773110a9fa3dd04d99b100e794c561874b82e9d
emerbe <michalbucki@gmail.com>
2014-07-08 15:04:54 +0200

    Update testFile.java

--- testFile.java
+++ testFile.java
@@ -4,5 +4,6 @@ public class testFile {
 	public static void main(String[] args) {
 		System.out.println("BEFORE REVERT");
 		System.out.println("MERGE");
+		System.out.println("Change from GitHub!");
 	}
 }
